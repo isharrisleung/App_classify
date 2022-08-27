5fb4e1b66b27ddce086fa6b7c3289cc0434a94a6
Harris <lianghaitao@HarrisdeMacBook-Pro.local>
2022-08-27 12:51:01 +0800

    mactest

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-python main.py main --model='GRU' \
+python main.py main --model='GRU_MULTI_HEAD' \
     --embedding_dim 100 --embedding_path "./data/wordvec_embsize_100_window_4_mincount_1.bin" \
     --attn_head 1 --device=0 --data_path='./data/new_train_data.csv'
 
