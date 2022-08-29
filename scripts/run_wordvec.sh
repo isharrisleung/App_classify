5bb907db1a840b82831249d973b9e4b26570a005
Harris <lianghaitao@HarrisdeMacBook-Pro.local>
2022-08-29 20:47:59 +0800

    add StratifiedKFold

--- scripts/run_wordvec.sh
+++ scripts/run_wordvec.sh
@@ -1,5 +1,5 @@
 python main.py main --model='Transformer_TextCNN' \
-    --embedding_dim 100 --vocab_size 2320 --embedding_path "./data/wordvec_embsize_100_window_4_mincount_2.bin" \
-    --attn_head 1 --device=0 --data_path='./data/wordvec_embsize_100_window_4_mincount_2.csv'
+    --embedding_dim 100 --vocab_size 2776 --embedding_path "./data/wordvec_embsize_100_window_4_mincount_1.bin" \
+    --attn_head 1 --device=0 --data_path='./data/good_data.csv'
 
 # python main.py main --model='Fasttext' --attn_head 1 --device=0 --data_path='./data/new_train_data.csv'
